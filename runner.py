dc32bbb892bb6e7403db4683de2f1bc8ab0d9cb1
koma <filip.komljenovic@fer.hr>
2021-03-02 11:35:19 +0100

    feat(basic-env): environment complete

--- runner.py
+++ runner.py
@@ -3,12 +3,13 @@ import numpy as np
 from environment.beer_game import BeerGame
 
 if __name__ == '__main__':
-    beer_game = BeerGame(n_agents=2)
+    beer_game = BeerGame(n_agents=4)
     start_state = beer_game.reset()
     # beer_game.render()
     done = False
     while not done:
-        actions = np.random.randint(0, 16, size=2)
+        actions = np.random.randint(0, 16, size=4)
         print(actions)
         step_state, step_rewards, done = beer_game.step(list(actions))
         beer_game.render()
+    print(beer_game.states)
